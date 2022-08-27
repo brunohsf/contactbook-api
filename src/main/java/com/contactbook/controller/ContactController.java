2d112baf6a95ddd3de95cac2812371be9ca71585
Bruno Henrique <brunohsfrancisco@gmail.com>
2022-08-27 16:09:53 -0300

    cria configuracao do swagger

--- src/main/java/com/contactbook/controller/ContactController.java
+++ src/main/java/com/contactbook/controller/ContactController.java
@@ -2,6 +2,8 @@ package com.contactbook.controller;
 
 import com.contactbook.entity.Contact;
 import com.contactbook.repository.ContactRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/contact")
+@Api(value = "API Rest for Contacts Management")
 @CrossOrigin("*")
 public class ContactController {
 
@@ -19,24 +22,28 @@ public class ContactController {
 
     @PostMapping(value = "/save")
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Insert contact in table contact")
     public Contact save(@RequestBody Contact contact) {
         return repository.save(contact);
     }
 
     @GetMapping(value = "/list")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Return a contact list")
     public List<Contact> list() {
         return repository.findAll();
     }
 
     @DeleteMapping("/delete/{idContato}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation("Delete a contact from the table")
     public void delete(@PathVariable("idContato") Integer idContato) {
         repository.deleteById(idContato);
     }
 
     @PutMapping("/update")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Update a contact from table contact")
     public Contact update(@RequestBody Contact contact) {
         return repository.save(contact);
     }
